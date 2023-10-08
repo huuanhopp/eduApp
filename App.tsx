e557ea757bd8d419f5ad9743f615059a76b40dae
Duy Nguyen <thanhduy5112020@gmail.com>
2023-10-08 23:58:14 +0700

    update puzzle games

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SpeakingGame2">
+      <Stack.Navigator initialRouteName="Start">
         {stackScreen.map(stackItem => (
           <Stack.Screen
             key={stackItem.name}
